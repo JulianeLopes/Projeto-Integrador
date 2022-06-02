10924df4448919e9d1738012c17f322f94e43a32
Juliane Lopes Moreira Rodrigues <juliane.lopes30@yahoo.com.br>
2022-06-02 17:34:31 -0300

    Instalacao do gerenciador de dependencias CocoaPods

---
+++ Pods/Target Support Files/Pods-ProjetoIntegrador-ProjetoIntegradorUITests/Pods-ProjetoIntegrador-ProjetoIntegradorUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ProjetoIntegrador_ProjetoIntegradorUITests : NSObject
+@end
+@implementation PodsDummy_Pods_ProjetoIntegrador_ProjetoIntegradorUITests
+@end
