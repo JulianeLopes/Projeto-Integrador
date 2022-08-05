a34883499db835967e9021a3d16e59bb4c5db9d3
Rafael Saugo <rafaelsaugo@gmail.com>
2022-08-05 19:54:38 -0300

    bug coreData

---
+++ Entities/UsuarioEntities+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  UsuarioEntities+CoreDataClass.swift
+//  
+//
+//  Created by Danielle Nozaki Ogawa on 2022/08/04.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(UsuarioEntities)
+public class UsuarioEntities: NSManagedObject {
+
+}
